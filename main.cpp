efbcd9511707a0f9bc31adffca3d778a16fda4b1
Tenzin <ten13zin@gmail.com>
2016-09-21 23:52:49 +0530

    Add C++ and HTML Media palyer cource code

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <windows.h>
+#include<string>
+
+using namespace std;
+
+void playMedia(int opt)
+{
+    char name[] = "";
+    if(opt == 1)
+       {
+           ShellExecute(NULL, "open", "video.html",
+            NULL, NULL, SW_SHOWNORMAL);
+       }
+    else
+        {
+            ShellExecute(NULL, "open", "audio.html",
+                NULL, NULL, SW_SHOWNORMAL);
+        }
+}
+
+int main()
+{
+
+   int opt;
+   cout<<"Media Player :"<<endl;
+   cout<<"1.Play Video\n2.Play Audio\n: ";
+   cin>>opt;
+   switch(opt)
+   {
+       case 1: playMedia(opt);
+                      break;
+
+       case 2: playMedia(opt);
+                      break;
+
+       default: cout<<"Invalid Option";
+   }
+
+   return 0;
+}
