f27dd1bbd912e742514f71d0a1d1ffce4fcc3ec1
Tenzin <ten13zin@gmail.com>
2016-09-25 22:15:57 +0530

    ""

--- main.cpp
+++ main.cpp
@@ -18,45 +18,7 @@ public:
          ShellExecute(NULL, "open", "audio.html",
             NULL, NULL, SW_SHOWNORMAL);
     }
-
-    void addMusic()
-    {
-        cout<<"Add Details of your favorite Music : "<<endl;
-        cout<<"Title : "; gets(title);
-        cout<<"Artist : "; gets(artist);
-        cout<<"Album : "; gets(album);
-        cout<<"Genera : "; gets(genera);
-        cout<<"Duration : "; cin>>duration;
-    }
-
-    void diskIn(int);
-    void diskOut();
-    static int diskCount();
 };
-
-void Music::diskIn(int mn)
-{
-    ifstream infile;
-    infile.open("SONGSLIB.DAT", ios::binary);
-    infile.seekg( mn*sizeof(Music) );
-    infile.read( (char *)this, sizeof(*this) );
-}
-
-void Music::diskOut()
-{
-    ofstream outfile;
-    outfile.open("SONGSLIB.DAT", ios::app | ios::binary);
-    outfile.write( (char*)this, sizeof(*this) );
-}
-
-int Music::diskCount()
-{
-    ifstream infile;
-    infile.open("SONGSLIB.DAT", ios::binary );
-    infile.seekg(0, ios::end );
-
-    return (int)infile.tellg() / sizeof(Music);
-}
 
 int main()
 {
