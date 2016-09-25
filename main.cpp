dc91fcf07c66feb3641c724e7c6d25233f6bede2
Tenzin <ten13zin@gmail.com>
2016-09-25 21:41:14 +0530

    Add Class music and Playmedia member

--- main.cpp
+++ main.cpp
@@ -1,43 +1,66 @@
 #include<iostream>
 #include <windows.h>
 #include<string>
-
-//testing fetch
+#include<fstream>
 using namespace std;
 
-void playMedia(int opt)
+class Music
 {
-    char name[] = "";
-    if(opt == 1)
-       {
-           ShellExecute(NULL, "open", "video.html",
+    char title[20];
+    char artist[20];
+    char album[40];
+    char genera[10];
+    int duration;
+
+public:
+    void playMedia()
+    {
+         ShellExecute(NULL, "open", "video.html",
             NULL, NULL, SW_SHOWNORMAL);
-       }
-    else
-        {
-            ShellExecute(NULL, "open", "audio.html",
-                NULL, NULL, SW_SHOWNORMAL);
-        }
+    }
+
+    void addMusic()
+    {
+        cout<<"Add Details of your favorite Music : "<<endl;
+        cout<<"Title : "; gets(title);
+        cout<<"Artist : "; gets(artist);
+        cout<<"Album : "; gets(album);
+        cout<<"Genera : "; gets(genera);
+        cout<<"Duration : "; cin>>duration;
+    }
+
+    void diskIn(int);
+    void diskOut();
+    static int diskCount();
+};
+
+void Music::diskIn(int mn)
+{
+    ifstream infile;
+    infile.open("SONGSLIB.DAT", ios::binary);
+    infile.seekg( mn*sizeof(Music) );
+    infile.read( (char *)this, sizeof(*this) );
+}
+
+void Music::diskOut()
+{
+    ofstream outfile;
+    outfile.open("SONGSLIB.DAT", ios::app | ios::binary);
+    outfile.write( (char*)this, sizeof(*this) );
+}
+
+int Music::diskCount()
+{
+    ifstream infile;
+    infile.open("SONGSLIB.DAT", ios::binary );
+    infile.seekg(0, ios::end );
+
+    return (int)infile.tellg() / sizeof(Music);
 }
 
 int main()
 {
-
-   int opt;
-   cout<<"lkfjasdlkjaskdjfssl"<<endl;
-   cout<<"Media Player :"<<endl;
-   cout<<"1.Play Video\n2.Play Audio\n: ";
-   cin>>opt;
-   switch(opt)
-   {
-       case 1: playMedia(opt);
-                      break;
-
-       case 2: playMedia(opt);
-                      break;
-
-       default: cout<<"Invalid Option";
-   }
-
+   Music m;
+   m.playMedia();
    return 0;
 }
